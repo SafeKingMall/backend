20ac6a57ec68415da3fb466a59fb6a7d117d9ed6
YooHayoung <yoo971202@naver.com>
2022-10-31 17:40:23 +0900

    feat : 로그인 요청시 OAuth2 요청 정보 저장
    OAuth2AuthorizationRequestRepository : 로그인 요청시 OAuth2 요청 정보를 쿠키로 저장, 쿠키에서 요청 파싱, 제거 등 역할
    
    CookieUtils : 쿠키 관련 작업을 처리하는 유틸리티 클래스

---
+++ src/main/java/com/safeking/shop/global/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.safeking.shop.global.config;
+
+import com.safeking.shop.global.security.oauth2.repository.OAuth2AuthorizationRequestRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final OAuth2AuthorizationRequestRepository oAuth2AuthorizationRequestRepository;
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> {
+            web.ignoring().antMatchers("/h2-console/**");
+        };
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .oauth2Login()
+                .authorizationEndpoint()
+                .baseUri("/oauth2/authorization/**")
+                .authorizationRequestRepository(oAuth2AuthorizationRequestRepository)
+
+                .and()
+                .redirectionEndpoint()
+                .baseUri("/oauth2/callback/*")
+
+                .and()
+                .userInfoEndpoint()
+                .userService()
+
+                .and()
+                .successHandler()
+                .failureHandler();
+
+        return http.build();
+    }
+}
