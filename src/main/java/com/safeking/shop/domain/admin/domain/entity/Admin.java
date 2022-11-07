9d36f5bb919365487bb7a90b28777380c9b293bd
minsung <kms199719@naver.com>
2022-11-07 14:52:19 +0900

    Feat "임시 커밋"

--- src/main/java/com/safeking/shop/domain/admin/domain/entity/Admin.java
+++ src/main/java/com/safeking/shop/domain/admin/domain/entity/Admin.java
@@ -14,12 +14,11 @@ public class Admin extends BaseTimeEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "admin_id")
     private Long id;
-
     private String loginId;
-
     private String password;
 
     public Admin(String loginId, String password) {
+
         this.loginId = loginId;
         this.password = password;
     }
