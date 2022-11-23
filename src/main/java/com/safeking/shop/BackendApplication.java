4e4575c21c57c9b27f150323e3e2aee2b45cefb9
minsung <kms199719@naver.com>
2022-11-23 20:56:22 +0900

    fix "임시커밋"

--- src/main/java/com/safeking/shop/BackendApplication.java
+++ src/main/java/com/safeking/shop/BackendApplication.java
@@ -1,13 +1,16 @@
 package com.safeking.shop;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import javax.persistence.EntityManager;
-
+@EnableBatchProcessing
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
