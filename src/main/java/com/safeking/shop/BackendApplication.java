ca7a25c21e73cd8e564c617ede2380c6df886e28
minsung <kms199719@naver.com>
2022-11-29 12:10:18 +0900

    fix "auditor 수정"

--- src/main/java/com/safeking/shop/BackendApplication.java
+++ src/main/java/com/safeking/shop/BackendApplication.java
@@ -1,6 +1,8 @@
 package com.safeking.shop;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.safeking.shop.global.auth.PrincipalDetails;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
@@ -14,14 +16,20 @@ import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.persistence.EntityManager;
+import java.util.Optional;
+
 @EnableBatchProcessing
 @EnableScheduling
 @SpringBootApplication
 @EnableJpaAuditing
+@Slf4j
 public class BackendApplication {
 
     public static void main(String[] args) {
@@ -33,6 +41,16 @@ public class BackendApplication {
         return new JPAQueryFactory(em);
     }
 
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
+        if (null == authentication || !authentication.isAuthenticated())return null;
+
+        log.info("--------"+authentication.getPrincipal());
+
+        PrincipalDetails principal = (PrincipalDetails) authentication.getPrincipal();
+        return ()->Optional.of(principal.getMember().getUsername());
+    }
 
 }
