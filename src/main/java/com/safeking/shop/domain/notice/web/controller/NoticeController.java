2b39140b59dbd0913a8b9a880cbcba27cbc8dce1
leedaesung <teseder5928@naver.com>
2022-12-02 17:46:12 +0900

    공지사항 상세, 목록 추가

--- src/main/java/com/safeking/shop/domain/notice/web/controller/NoticeController.java
+++ src/main/java/com/safeking/shop/domain/notice/web/controller/NoticeController.java
@@ -11,6 +11,7 @@ import com.safeking.shop.domain.notice.domain.sevice.NoticeService;
 import com.safeking.shop.domain.notice.domain.sevice.servicedto.notice.NoticeSaveDto;
 import com.safeking.shop.domain.notice.domain.sevice.servicedto.notice.NoticeUpdateDto;
 import com.safeking.shop.domain.notice.domain.sevice.servicedto.notice.NoticeViewDto;
+import com.safeking.shop.domain.notice.web.response.NoticeListResponse;
 import com.safeking.shop.domain.notice.web.response.NoticeViewResponse;
 import com.safeking.shop.global.jwt.TokenUtils;
 import lombok.RequiredArgsConstructor;
@@ -54,6 +55,31 @@ public class NoticeController {
     }
 
     @GetMapping("/admin/notice/{noticeId}")
+    public NoticeViewResponse adminView(@PathVariable Long noticeId){
+        NoticeViewDto noticeViewDto = noticeService.view(noticeId);
+        return new NoticeViewResponse(noticeViewDto.getId(),
+                noticeViewDto.getTitle(),
+                noticeViewDto.getContents(),
+                noticeViewDto.getMemberId(),
+                noticeViewDto.getCreateDate(),
+                noticeViewDto.getLastModifiedDate()
+        );
+    }
+
+    @GetMapping("/admin/notice/list")
+    public Page<NoticeListResponse> adminList(Pageable pageable){
+        Page<NoticeListResponse> page = noticeService.list(pageable).map(m->NoticeListResponse.builder()
+                .id(m.getId())
+                .title(m.getTitle())
+                .memberId(m.getMemberId())
+                .createDate(m.getCreateDate().toString())
+                .lastModifiedDate(m.getLastModifiedDate().toString())
+                .build()
+        );
+        return page;
+    }
+
+    @GetMapping("/notice/{noticeId}")
     public NoticeViewResponse view(@PathVariable Long noticeId){
         NoticeViewDto noticeViewDto = noticeService.view(noticeId);
         return new NoticeViewResponse(noticeViewDto.getId(),
@@ -63,6 +89,19 @@ public class NoticeController {
                 noticeViewDto.getCreateDate(),
                 noticeViewDto.getLastModifiedDate()
         );
+    }
+
+    @GetMapping("/notice/list")
+    public Page<NoticeListResponse> list(Pageable pageable){
+        Page<NoticeListResponse> page = noticeService.list(pageable).map(m->NoticeListResponse.builder()
+                .id(m.getId())
+                .title(m.getTitle())
+                .memberId(m.getMemberId())
+                .createDate(m.getCreateDate().toString())
+                .lastModifiedDate(m.getLastModifiedDate().toString())
+                .build()
+        );
+        return page;
     }
 
 }
