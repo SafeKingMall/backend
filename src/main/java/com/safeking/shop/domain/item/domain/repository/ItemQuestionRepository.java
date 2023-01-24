8ace98aceedc4603002e476a54f0e51c6a416905
leedaeseong <teseder5928@naver.com>
2023-01-24 21:17:42 +0900

    itemQna 검색기능 추가

--- src/main/java/com/safeking/shop/domain/item/domain/repository/ItemQuestionRepository.java
+++ src/main/java/com/safeking/shop/domain/item/domain/repository/ItemQuestionRepository.java
@@ -3,29 +3,26 @@ package com.safeking.shop.domain.item.domain.repository;
 import com.querydsl.core.group.GroupBy;
 import com.safeking.shop.domain.item.domain.entity.Item;
 import com.safeking.shop.domain.item.domain.entity.ItemQuestion;
-import com.safeking.shop.domain.user.domain.entity.member.Member;
+import com.safeking.shop.domain.notice.domain.entity.Notice;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.domain.Page;
 
-import java.util.List;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface ItemQuestionRepository extends JpaRepository<ItemQuestion,Long> {
 
 
     @Override
-    @Query("select iq from ItemQuestion iq join fetch iq.writer where iq.id=:id")
+    @Query("select iq from ItemQuestion iq join fetch iq.member where iq.id=:id")
     Optional<ItemQuestion> findById(@Param("id") Long id);
 
     Page<ItemQuestion> findByTitleContaining(Pageable pageable, String title);
 
-    List<ItemQuestion> findByWriter(Member writer);
+    Page<ItemQuestion> findByMemberUsername(Pageable pageable, String memberId);
 
-    @Modifying
-    @Query("delete from ItemQuestion iq where iq in :questionList")
-    void deleteByTargetBatch(@Param("questionList") List<ItemQuestion> questionList);
+    Page<ItemQuestion> findByCreateDateBetween(Pageable pageable, LocalDateTime startDateTime, LocalDateTime endDateTime);
 }
