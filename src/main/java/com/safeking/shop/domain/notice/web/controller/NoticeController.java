d6c9d6caa2b0d55f55b8726c49eedb8a458368a4
leedaeseong <teseder5928@naver.com>
2023-01-31 00:16:10 +0900

    Notice 이전글, 다음글 수정

--- src/main/java/com/safeking/shop/domain/notice/web/controller/NoticeController.java
+++ src/main/java/com/safeking/shop/domain/notice/web/controller/NoticeController.java
@@ -169,7 +169,7 @@ public class NoticeController {
 
     @GetMapping("admin/notice/prev/{noticeId}")
     public NoticeViewResponse adminViewPrev(@PathVariable Long noticeId, @RequestParam(required = false) String title
-            , @RequestParam(required = false) String createDate, Pageable pageable){
+            , @RequestParam(required = false) String createDate, @PageableDefault(size=1000000) Pageable pageable){
         NoticeViewDto noticeViewDto = noticeService.viewPrev(noticeId, title, createDate, pageable);
         if(noticeViewDto == null) return null;
         return new NoticeViewResponse(noticeViewDto.getId(),
@@ -183,7 +183,35 @@ public class NoticeController {
 
     @GetMapping("admin/notice/next/{noticeId}")
     public NoticeViewResponse adminViewNext(@PathVariable Long noticeId, @RequestParam(required = false) String title
-            , @RequestParam(required = false) String createDate, Pageable pageable){
+            , @RequestParam(required = false) String createDate, @PageableDefault(size=1000000) Pageable pageable){
+        NoticeViewDto noticeViewDto = noticeService.viewNext(noticeId, title, createDate, pageable);
+        if(noticeViewDto == null) return null;
+        return new NoticeViewResponse(noticeViewDto.getId(),
+                noticeViewDto.getTitle(),
+                noticeViewDto.getContents(),
+                noticeViewDto.getMemberId(),
+                noticeViewDto.getCreateDate(),
+                noticeViewDto.getLastModifiedDate()
+        );
+    }
+
+    @GetMapping("notice/prev/{noticeId}")
+    public NoticeViewResponse viewPrev(@PathVariable Long noticeId, @RequestParam(required = false) String title
+            , @RequestParam(required = false) String createDate, @PageableDefault(size=1000000) Pageable pageable){
+        NoticeViewDto noticeViewDto = noticeService.viewPrev(noticeId, title, createDate, pageable);
+        if(noticeViewDto == null) return null;
+        return new NoticeViewResponse(noticeViewDto.getId(),
+                noticeViewDto.getTitle(),
+                noticeViewDto.getContents(),
+                noticeViewDto.getMemberId(),
+                noticeViewDto.getCreateDate(),
+                noticeViewDto.getLastModifiedDate()
+        );
+    }
+
+    @GetMapping("notice/next/{noticeId}")
+    public NoticeViewResponse viewNext(@PathVariable Long noticeId, @RequestParam(required = false) String title
+            , @RequestParam(required = false) String createDate, @PageableDefault(size=1000000) Pageable pageable){
         NoticeViewDto noticeViewDto = noticeService.viewNext(noticeId, title, createDate, pageable);
         if(noticeViewDto == null) return null;
         return new NoticeViewResponse(noticeViewDto.getId(),
